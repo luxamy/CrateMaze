6657d382da61f19937f90ba3ed8a81c258363caa
kathi <kathi@nagl-online.de>
2016-12-23 11:13:48 +0100

    Highscore Klassen (nur erstellt bisher mit 1 Bild)

---
+++ app/src/main/java/com/cratemaze/kark/cratemaze/HighscoreActivity.java
@@ -0,0 +1,23 @@
+package com.cratemaze.kark.cratemaze;
+
+import android.content.pm.ActivityInfo;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+/**
+ * Created by consult on 23.12.2016.
+ */
+public class HighscoreActivity
+{
+    private DatabaseManager dbmgr;
+    private SQLiteDatabase sqldb;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_selection);
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //titel.setImageResource(R.mipmap.highscore);
+    }
+}
