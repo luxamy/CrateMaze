abcf124bdf3c4e65440622150528861d0ffae3db
rk <robert_kais@gmx.de>
2017-02-09 10:00:05 +0100

    highscore shows up even if there are empty highscores

--- app/src/main/java/com/cratemaze/kark/cratemaze/HighscoreActivity.java
+++ app/src/main/java/com/cratemaze/kark/cratemaze/HighscoreActivity.java
@@ -22,59 +22,43 @@ public class HighscoreActivity extends Activity implements View.OnClickListener
 
         dbmgr = new DatabaseManager(this);
 
-        final TextView level1  = (TextView) findViewById(R.id.fScore1);
-        final TextView level2  = (TextView) findViewById(R.id.fScore2);
-        final TextView level3  = (TextView) findViewById(R.id.fScore3);
-        final TextView level4  = (TextView) findViewById(R.id.fScore4);
-        final TextView level5  = (TextView) findViewById(R.id.fScore5);
-        final TextView level6  = (TextView) findViewById(R.id.fScore6);
-        final TextView level7  = (TextView) findViewById(R.id.fScore7);
-        final TextView level8  = (TextView) findViewById(R.id.fScore8);
-        final TextView level9  = (TextView) findViewById(R.id.fScore9);
-        final TextView level10 = (TextView) findViewById(R.id.fScore10);
-
-        final TextView level1score  = (TextView) findViewById(R.id.fScore1_1);
-        final TextView level2score  = (TextView) findViewById(R.id.fScore2_1);
-        final TextView level3score  = (TextView) findViewById(R.id.fScore3_1);
-        final TextView level4score  = (TextView) findViewById(R.id.fScore4_1);
-        final TextView level5score  = (TextView) findViewById(R.id.fScore5_1);
-        final TextView level6score  = (TextView) findViewById(R.id.fScore6_1);
-        final TextView level7score  = (TextView) findViewById(R.id.fScore7_1);
-        final TextView level8score  = (TextView) findViewById(R.id.fScore8_1);
-        final TextView level9score  = (TextView) findViewById(R.id.fScore9_1);
-        final TextView level10score = (TextView) findViewById(R.id.fScore10_1);
-
-        level1.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 0)) - 1));
-        level1score.setText(dbmgr.ausgabe("level", "time", 0));
-
-        level2.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 1)) - 1));
-        level2score.setText(dbmgr.ausgabe("level", "time", 1));
-
-        level3.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 2)) - 1));
-        level3score.setText(dbmgr.ausgabe("level", "time", 2));
-
-        level4.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 3)) - 1));
-        level4score.setText(dbmgr.ausgabe("level", "time", 3));
-
-        level5.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 4)) - 1));
-        level5score.setText(dbmgr.ausgabe("level", "time", 4));
-
-        level6.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 5)) - 1));
-        level6score.setText(dbmgr.ausgabe("level", "time", 5));
-
-        level7.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 6)) - 1));
-        level7score.setText(dbmgr.ausgabe("level", "time", 6));
-
-        level8.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 7)) - 1));
-        level8score.setText(dbmgr.ausgabe("level", "time", 7));
-
-        level9.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 8)) - 1));
-        level9score.setText(dbmgr.ausgabe("level", "time", 8));
-
-        level10.setText(dbmgr.ausgabe("player", "name", Integer.parseInt(dbmgr.ausgabe("level", "highscore", 9)) - 1));
-        level10score.setText(dbmgr.ausgabe("level", "time", 9));
-
         final Button bBack = (Button) findViewById(R.id.b_highscore_back);
+
+        TextView player[] = new TextView[10];
+        TextView score[] = new TextView[10];
+
+        player[0] = (TextView) findViewById(R.id.fScore1);
+        player[1] = (TextView) findViewById(R.id.fScore2);
+        player[2] = (TextView) findViewById(R.id.fScore3);
+        player[3] = (TextView) findViewById(R.id.fScore4);
+        player[4] = (TextView) findViewById(R.id.fScore5);
+        player[5] = (TextView) findViewById(R.id.fScore6);
+        player[6] = (TextView) findViewById(R.id.fScore7);
+        player[7] = (TextView) findViewById(R.id.fScore8);
+        player[8] = (TextView) findViewById(R.id.fScore9);
+        player[9] = (TextView) findViewById(R.id.fScore10);
+
+        score[0] = (TextView) findViewById(R.id.fScore1_1);
+        score[1] = (TextView) findViewById(R.id.fScore2_1);
+        score[2] = (TextView) findViewById(R.id.fScore3_1);
+        score[3] = (TextView) findViewById(R.id.fScore4_1);
+        score[4] = (TextView) findViewById(R.id.fScore5_1);
+        score[5] = (TextView) findViewById(R.id.fScore6_1);
+        score[6] = (TextView) findViewById(R.id.fScore7_1);
+        score[7] = (TextView) findViewById(R.id.fScore8_1);
+        score[8] = (TextView) findViewById(R.id.fScore9_1);
+        score[9] = (TextView) findViewById(R.id.fScore10_1);
+
+        for(int i = 0; i <= dbmgr.getCount("level"); i++)
+        {
+            String playerid = dbmgr.ausgabe("level", "highscore", i);
+            if(playerid.equals("null"))
+                continue;
+
+            int tmp = Integer.parseInt(playerid) - 1;
+            player[i].setText(dbmgr.ausgabe("player", "name", tmp));
+            score[i].setText(dbmgr.ausgabe("level", "time", i));
+        }
 
         bBack.setOnClickListener(this);
     }
